49dd8c0ceaf815bc3f6a024413508a5bf67cd3a1
aoking21 <ayky3305@gmail.com>
2021-06-26 17:38:46 +0900

    各種ファイルを追加

---
+++ switch.ino
@@ -0,0 +1,29 @@
+////////////////////////////////////////////
+// Arduino Leonardoから
+// ショートカットキー [cmd + shift + m] を送る
+////////////////////////////////////////////
+#include <Keyboard.h>
+
+#define ButtonPin 9
+
+void setup()
+{
+  Keyboard.begin();
+  pinMode(ButtonPin, INPUT_PULLUP);
+}
+
+void loop()
+{
+  if (digitalRead(ButtonPin) == LOW) // cmd + shift + m を送る
+  {
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press('m');
+    delay(100);
+    Keyboard.releaseAll();
+
+    while (digitalRead(ButtonPin) == LOW);
+  }
+  delay(100);
+}
